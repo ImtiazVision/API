e645880320afcbbda74b189452de26af723e224c
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2021-09-21 16:00:54 -0400

    update on api req

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var request = new XMLHttpRequest();
 
 request.open('GET', 'https://hplussport.com/api/products');
+// The base URL allows us to connect to the API and get a response full of data.We can filter the data by using parameter.
+request.open('GET', 'https://hplussport.com/api/products/qty/2/order/name');
 
 // request.onload will trigger the function
 request.onload = function() {
