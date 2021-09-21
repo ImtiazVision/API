080100fc7f080041bf6d78bc8303d3c31b4e2a79
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2021-09-21 15:53:36 -0400

    made api request

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 var request = new XMLHttpRequest();
 
-request.open('GET', 'https://hplussport.com/api/products')
+request.open('GET', 'https://hplussport.com/api/products');
+
+// request.onload will trigger the function
+request.onload = function() {
+  // request.response will prompt the API to response 
+  var response = request.response;
+  // normally api will return a string, but we are using JSON.parse to convert it to a JavaScript object
+  var parsedData = JSON.parse(response);
+  console.log(parsedData);
+}
+
+// sending the request
+request.send();
